6852fb8f9e398724c10ddab43e4de3f35d23e334
Mark A. Matney, Jr <markmatney@protonmail.com>
2022-08-10 20:08:32 -0700

    Add tests and Surefire configuration to demonstrate the issue

--- src/test/java/com/github/markmatney/vertx_junit5_issues/TestMainVerticle.java
+++ src/test/java/com/github/markmatney/vertx_junit5_issues/TestMainVerticle.java
@@ -2,7 +2,11 @@ package com.github.markmatney.vertx_junit5_issues;
 
 import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxParameterProvider;
 import io.vertx.junit5.VertxTestContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +21,25 @@ public class TestMainVerticle {
 
   @Test
   void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    testContext.completeNow();
+  }
+
+  @Test
+  void vertx_parameter_filename(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    String filename = System.getenv(VertxParameterProvider.VERTX_PARAMETER_FILENAME);
+
+    assertNotNull(filename);
+
+    testContext.completeNow();
+  }
+
+  @Test
+  void vertx_parameter_filename_portable(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    String portableVariableName = VertxParameterProvider.VERTX_PARAMETER_FILENAME.replace('.', '_').toUpperCase();
+    String filename = System.getenv(portableVariableName);
+
+    assertNotNull(filename);
+
     testContext.completeNow();
   }
 }
